8aad1017c2c47ab69aee5578b9b871be7ff6cdf0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-23 15:01:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Toaster } from "@sonner/react";
+import { Toaster } from "sonner";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Home from "./pages/Home";
