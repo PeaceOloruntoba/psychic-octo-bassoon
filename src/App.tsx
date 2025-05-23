df4129c16e42bfbd056d7d04b2bda614701e5714
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-23 14:59:40 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "@sonner/react";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Home from "./pages/Home";
+import MemoPage from "./pages/MemoPage";
+import ArchivePage from "./pages/ArchivePage";
+import AdminPage from "./pages/AdminPage";
+import Navbar from "./components/ui/Navbar";
+import { useAuthStore } from "./stores/authStore";
+
+function App() {
+  const { user } = useAuthStore();
+
   return (
-    <div className="w-screen h-screen bg-black text-white">App</div>
-  )
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        {user && <Navbar />}
+        <Toaster richColors />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/memos" element={<MemoPage />} />
+          <Route path="/archive" element={<ArchivePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
